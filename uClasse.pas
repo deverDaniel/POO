cc1ea8bcf6ff98ab2736b1baf59af65599b14280
DanielFerreira <dever.daniel.asf@gmail.com>
2025-07-28 17:02:07 -0300

    adicionando classe TPersonagem e TPlayer

--- uClasse.pas
+++ uClasse.pas
@@ -2,6 +2,31 @@ unit uClasse;
 
 interface
 
+  //Abstração
+  //Encapsulamento
+  type TPersonagem = class
+    Nome, Genero: String;
+    Vida, Nivel, Dano, Defesa: Integer;
+    constructor Create (aDano, aDefesa: Integer);
+  end;
+
+  //herança
+  type Tplayer = class (TPersonagem)
+    NickName: String;
+    Experiencia: Double;
+  end;
+  type TBoss = class (TPersonagem)
+
+  end;
+
 implementation
+
+{ TPersonagem }
+
+constructor TPersonagem.Create(aDano, aDefesa: Integer);
+begin
+  Self.Dano:= aDano;
+  Self.Defesa:= aDefesa;
+end;
 
 end.
